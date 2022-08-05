71c0a306821e352d341bef1897235f3234942d35
Leonardo Gonçalves <leonardo@lginfo.com.br>
2022-08-04 22:56:56 -0300

    Classes Bootcamp e Dev

---
+++ src/br/com/lginfo/desafio/dominio/Dev.java
@@ -0,0 +1,47 @@
+package br.com.lginfo.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+    public void inscreverBootCamp(BootCamp bootCamp){
+
+    }
+
+    public void progredir(){
+
+    }
+
+    public void calcularTotalXp(){
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+}
