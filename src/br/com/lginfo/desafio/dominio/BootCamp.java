87cf414f05bbe88c7a3a06f8684ad5b20d3dbdf3
Leonardo Gonçalves <leonardo@lginfo.com.br>
2022-08-04 23:22:32 -0300

    add metodos a classe Dev

--- src/br/com/lginfo/desafio/dominio/BootCamp.java
+++ src/br/com/lginfo/desafio/dominio/BootCamp.java
@@ -2,6 +2,7 @@ package br.com.lginfo.desafio.dominio;
 
 import java.time.LocalDate;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -11,6 +12,7 @@ public class BootCamp {
     private final LocalDate dataInicio = LocalDate.now();
     private final LocalDate dataFinal  = dataInicio.plusDays(45);
     private Set<Dev> devsInscritos = new HashSet<>();
+    private Set<Conteudo> conteudos = new LinkedHashSet<>();
 
     public String getNome() {
         return nome;
@@ -39,9 +41,16 @@ public class BootCamp {
     public Set<Dev> getDevsInscritos() {
         return devsInscritos;
     }
-
     public void setDevsInscritos(Set<Dev> devsInscritos) {
         this.devsInscritos = devsInscritos;
+    }
+
+    public Set<Conteudo> getConteudos() {
+        return conteudos;
+    }
+
+    public void setConteudos(Set<Conteudo> conteudos) {
+        this.conteudos = conteudos;
     }
 
     @Override
--- src/br/com/lginfo/desafio/dominio/Dev.java
+++ src/br/com/lginfo/desafio/dominio/Dev.java
@@ -1,6 +1,7 @@
 package br.com.lginfo.desafio.dominio;
 
 import java.util.LinkedHashSet;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -9,15 +10,24 @@ public class Dev {
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
     public void inscreverBootCamp(BootCamp bootCamp){
-
+            this.conteudosInscritos.addAll(bootCamp.getConteudos());
+            bootCamp.getDevsInscritos().add(this);
     }
-
     public void progredir(){
-
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()){
+                this.conteudosConcluidos.add(conteudo.get());
+                this.conteudosInscritos.remove(conteudo.get());
+        }else{
+            System.err.println("Você não está matriculado em nenhum Bootcamp");
+        }
     }
 
-    public void calcularTotalXp(){
-
+    public double calcularTotalXp(){
+        return this.conteudosConcluidos
+                .stream()
+                .mapToDouble(Conteudo::calcularXp)
+                .sum();
     }
 
     public String getNome() {
